759cddedf793299a948f9349965903f0d0b01477
Rohitpari <rohitprihar7049@gmail.com>
2024-11-22 15:14:16 +0000

    conversion of data type

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "rohitprihar7049@gmail.com"
+var accoundPassword = "12345"
+accoundCity = "jaipur"//use nahi karna hai 
+let accountstate;
+/*
+prefer not to use var 
+becouse of issue in block scope and functional scope
+*/
+// accountId = 2  //not allowed
+// accountId = "113113"
+accountEmail = "hc@hc.com"
+accoundPassword = "121212"
+accoundCity = "hjhcajlddil"
+console.table([accountId,accountEmail,accoundPassword,accoundCity,accountstate])
+// console.log(accountId);
---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,26 @@
+let score = "33" //33 "33" undefined  true 
+// console.log(typeof(score))
+// console.log(typeof score)
+/*
+"33" => 33
+"33abc"=>nan
+true =>1; false =>0;
+
+*/
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))//string to number main chance kiya 
+// console.log(valueInNumber); //print NAN not a number
+
+
+let isloggedin = "";
+let booleanisloggedIn = Boolean(isloggedin);
+// console.log(booleanisloggedIn)
+// console.log(typeof(booleanisloggedIn))
+//1=>true and 0=>false 
+//"rohit"=>true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof(stringnumber))
